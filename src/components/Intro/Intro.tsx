3f5602a00fbc25f3dac6987ea87b2c830a86b018
Suzy <suzy.nakayama@gmail.com>
2020-06-24 16:42:25 -0400

    added sample to intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div>
-      INTRO
+    <div className="intro">
+      <video src="#" className="intro-video"></video>
+      <h2 className="intro-title">Anna Tokiko</h2>
+      <p className="intro-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae reiciendis doloribus illo! Eaque nisi explicabo consequuntur dolorum natus dolorem laboriosam reiciendis necessitatibus optio maiores cumque esse, dolor, voluptates omnis quia!</p>
     </div>
   )
 }
